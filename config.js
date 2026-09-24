4725aa696a228bb3af808ea500dd9cdafc40dced
asterisk3532-art <asterisk3532@gmail.com>
2026-09-24 16:23:21 +0900

    Change CLIENT_ID in config.js
    Updated CLIENT_ID to the new OAuth client ID.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ここだけ自分の値に書き換える
 window.DIARY_CONFIG = {
   // Google Cloud で作った「ウェブ アプリケーション」の OAuth クライアント ID
-  CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: 313904888310-6cqc1b4om66ek907a2b9ggtbdp9qu180.apps.googleusercontent.com,
   // Google ドライブの「日記」フォルダの ID（作成済み）
   FOLDER_ID: "1zLPwCslA6aSUCqZxtNmmJrUoXJn3aXae",
   // ログインに使う Google アカウント（省略可）
